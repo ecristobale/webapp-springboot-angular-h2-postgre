ee20562c437f3631af58a1c66e52f3115c48a9f3
ecristobale <edu_ce_1988@hotmail.com>
2019-11-24 18:05:33 +0100

    Rest API: .toString method added to MyEntity.java

--- RestAPI/src/main/java/com/ecristobale/microserv/restapi/models/MyEntity.java
+++ RestAPI/src/main/java/com/ecristobale/microserv/restapi/models/MyEntity.java
@@ -31,4 +31,9 @@ public class MyEntity {
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	@Override
+	public String toString() {
+		return "MyEntity [id=" + id + ", name=" + name 
+				+ ", description=" + description + "]";
+	}
 }
